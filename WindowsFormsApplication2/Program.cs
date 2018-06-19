4f718939d360d2b37b773b9239b1ebb9a3224919
Богдан Валовый <bogdan10075@gmail.com>
2018-06-19 22:07:09 +0300

    //HACK: круг - радиус //HACK: квадрат - стороны //HACK: несколько фигур на полотне //HACK: регулировка толщины линий //HACK: побольше панель

--- WindowsFormsApplication2/Program.cs
+++ WindowsFormsApplication2/Program.cs
@@ -9,7 +9,7 @@ namespace WindowsFormsApplication2
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// Главная точка  входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
